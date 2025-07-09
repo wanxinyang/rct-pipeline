972c1f67c27db39747ba5d5d850c6fe96d90d06e
wanxinyang <w.yang.17@ucl.ac.uk>
2025-07-09 17:20:24 +0100

    Update input argument so that it applies to all run_*.sh

--- batch_run_rct_parallel.py
+++ batch_run_rct_parallel.py
@@ -2,39 +2,43 @@ import os
 import subprocess
 import argparse
 from multiprocessing import Pool, cpu_count
+import glob
 
-def process_file(args):
-    script_path, input_dir, ply_file = args
-    full_path = os.path.join(input_dir, ply_file)
-    print(f"Processing {ply_file}...")
+SCRIPT_PATH = None
+
+def process_file(file_path):
+    print(f"Processing {file_path}...")
     try:
-        subprocess.run([script_path, input_dir, ply_file], check=True)
+        subprocess.run([SCRIPT_PATH, file_path], check=True)
     except subprocess.CalledProcessError as e:
-        print(f"Error occurred while processing {ply_file}: {e}")
+        print(f"Error occurred while processing {file_path}: {e}")
 
-def main(input_dir, script_path):
-    # Search for all .ply files whose filenames do not contain '_raycloud'
-    ply_files = [f for f in os.listdir(input_dir) if f.endswith(".ply") and "_raycloud" not in f]
-
-    if not ply_files:
-        print("No .ply files found excluding '_raycloud' in the filename in the specified directory.")
+def main(patterns, script_path):
+    # Expand input patterns to absolute file paths
+    file_paths = []
+    for pat in patterns:
+        file_paths.extend(glob.glob(pat))
+    if not file_paths:
+        print("No matching .ply files found.")
         return
+    # get aboslute paths
+    file_paths = [os.path.abspath(fp) for fp in file_paths if os.path.isfile(fp)]
+    file_paths.sort()
 
-    # Sort the files for better readability, in case order matters
-    ply_files.sort()
-
-    # Prepare arguments for multiprocessing
-    args_list = [(script_path, input_dir, ply_file) for ply_file in ply_files]
+    global SCRIPT_PATH
+    SCRIPT_PATH = script_path
 
     # Use multiprocessing to process files in parallel
     num_cores = cpu_count()
     with Pool(num_cores) as pool:
-        pool.map(process_file, args_list)
+        pool.map(process_file, file_paths)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Batch run the bash script for multiple .ply files.")
-    parser.add_argument("-i", "--input", required=True, help="Path to the directory containing the .ply files.")
-    parser.add_argument("-s", "--script", required=True, help="Path to the run_rct.sh script.")
+    parser.add_argument("-i", "--input", nargs="+", 
+                        help="Single or multiple input filename(s)")
+    parser.add_argument("-s", "--script_path", required=True, 
+                        help="Path to the rct script, can be 'rayextract_on_raycloud.sh', 'rayextract_on_nonraycloud.sh', or 'treesplit_and_treemesh.sh'")
     args = parser.parse_args()
 
-    main(args.input, args.script)
+    main(args.input, args.script_path)
