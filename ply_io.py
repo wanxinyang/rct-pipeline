a0ffc4302877673267e4f574bcaa240620a029b7
wanxinyang <w.yang.17@ucl.ac.uk>
2026-05-12 14:54:46 +0100

    Add float-precision PLY writer alongside existing double writer.
    - change write_ply() for float32 (property float) output
    - add write_ply_double() for float64 (property double) output
    - allow choosing precision mode from one shared ply_io.py module

--- ply_io.py
+++ ply_io.py
@@ -51,6 +51,42 @@ def read_ply_(fp, newline):
 def write_ply(output_name, pc, comments=[]):
 
     cols = ['x', 'y', 'z']
+    pc[['x', 'y', 'z']] = pc[['x', 'y', 'z']].astype('f4')
+
+    with open(output_name, 'w') as ply:
+
+        ply.write("ply\n")
+        ply.write('format binary_little_endian 1.0\n')
+        ply.write("comment Author: Phil Wilkes\n")
+        for comment in comments:
+            ply.write("comment {}\n".format(comment))
+        ply.write("obj_info generated with pcd2ply.py\n")
+        ply.write("element vertex {}\n".format(len(pc)))
+        ply.write("property float x\n")
+        ply.write("property float y\n")
+        ply.write("property float z\n")
+        if 'red' in pc.columns:
+            cols += ['red', 'green', 'blue']
+            pc[['red', 'green', 'blue']] = pc[['red', 'green', 'blue']].astype('i')
+            ply.write("property int red\n")
+            ply.write("property int green\n")
+            ply.write("property int blue\n")
+        for col in pc.columns:
+            if col in cols: continue
+            try:
+                pc[col] = pc[col].astype('f4')
+                ply.write("property float {}\n".format(col))
+                cols += [col]
+            except:
+                pass
+        ply.write("end_header\n")
+
+    with open(output_name, 'ab') as ply:
+        ply.write(pc[cols].to_records(index=False).tobytes()) 
+
+def write_ply_double(output_name, pc, comments=[]):
+
+    cols = ['x', 'y', 'z']
     pc[['x', 'y', 'z']] = pc[['x', 'y', 'z']].astype('f8')
 
     with open(output_name, 'w') as ply:
@@ -62,9 +98,6 @@ def write_ply(output_name, pc, comments=[]):
             ply.write("comment {}\n".format(comment))
         ply.write("obj_info generated with pcd2ply.py\n")
         ply.write("element vertex {}\n".format(len(pc)))
-        # ply.write("property float64 x\n")
-        # ply.write("property float64 y\n")
-        # ply.write("property float64 z\n")
         ply.write("property double x\n")
         ply.write("property double y\n")
         ply.write("property double z\n")
@@ -78,7 +111,6 @@ def write_ply(output_name, pc, comments=[]):
             if col in cols: continue
             try:
                 pc[col] = pc[col].astype('f8')
-                # ply.write("property float64 {}\n".format(col))
                 ply.write("property double {}\n".format(col))
                 cols += [col]
             except:
